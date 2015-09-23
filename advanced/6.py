a40ff34ec6c7837df05d6202d841dbe824a8afd6
hoanvu <vumaihoan@gmail.com>
2015-09-23 09:55:37 +0700

    Add advanced/6.py

---
+++ advanced/6.py
@@ -0,0 +1,12 @@
+'''Find the difference between the sum of the squares of 
+the first 100 natural numbers and the square of the sum
+'''
+# Limit
+N = 100
+
+sum = (N * ( N + 1 )) / 2
+square_of_sum = sum * sum
+
+sum_of_squares = ( N * ( N + 1 ) * ( 2 * N + 1 ) ) / 6
+
+print square_of_sum - sum_of_squares
