22481abc15210ba263f13b52665a7793edb658cb
hoanvu <vumaihoan@gmail.com>
2015-09-24 10:58:45 +0700

    add prob_9 & prob_13

---
+++ prob_9_solution_1.py
@@ -0,0 +1,6 @@
+# There exists exactly one Pythagorean triplet a,b,c for which a + b + c = 1000
+# Find prodct abc
+
+# A Pythagorean triplet is a set of 3 numbers a < b < c for which 
+# square(a) + square(b) = square(c)
+
