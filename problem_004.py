554ce5b1497db95b3422ecaca17fcec4f7f07d5f
hoanvu <vumaihoan@gmail.com>
2016-06-29 23:33:25 +0700

    update logic for problem_004

--- problem_004.py
+++ problem_004.py
@@ -4,29 +4,8 @@
 
 # Check whether a number is a palindrome by keep dividing it by 10
 # and get the remainder
-def isPalindrome(inputNumber):
-	reverse = ""	# This string store the reversed of the input number
-
-	# Store the input number to a temp variable
-	# We need the inputNumber for last comparison
-	temp = inputNumber 
-
-	# As long as the number has more than 1 digits
-	while temp > 10:
-		# Find the last number by modulo, then convert to string, and add to 'reverse'
-		reverse += str(temp % 10)
-		temp /= 10	# then divide the number by 10 to remove the last number
-
-	# Add the last number to the reverse
-	reverse += str(temp)
-	# Convert 'reverse' to integer
-	reverse = int(reverse)
-
-	# If reverse equals to input, then palindrome, 
-	if reverse == inputNumber:
-		return True
-	# otherwise
-	return False
+def is_palindrome(number):
+	return True if str(number) == str(number)[::-1] else False
 
 # Keep multiplying 2 3-digit numbers until we find a largest palindrome
 def main():
@@ -34,7 +13,6 @@ def main():
 	# palindrome: 		store the largest palindrome we found so far
 	# Do not use list for memory efficiency
 	palindrome = -1
-	currPalindrome = -1
 
 	# Use 2 for loop, multiply all numbers between 100 and 999
 	# Check for palindrome after each multiplication
@@ -43,13 +21,13 @@ def main():
 			product = number1 * number2
 
 			# If a product is a palindrome, check if it's the largest palindrome so far
-			if isPalindrome(product):
-				currPalindrome = product
-				if currPalindrome > palindrome:
-					palindrome = currPalindrome
+			if is_palindrome(product):
+				curr_palindrome = product
+				if curr_palindrome > palindrome:
+					palindrome = curr_palindrome
 
 	# Print only the largest palindrome
-	print palindrome
+	print (palindrome)
 
 if __name__ == "__main__":
 	main()
