a14edc235ea489d21017d8a09653576651b6f818
hoanvu <vumaihoan@gmail.com>
2016-06-30 10:32:05 +0700

    update code for problem 04

---
+++ problem_004_solution_01.py
@@ -0,0 +1,29 @@
+# Calculate the largest palindrome number made by product of 2 3-digit numbers
+# A palindrome number is a number that read the same both ways. Eg: 8118, 93239, ...
+# =================================================================================== #
+
+# Check whether a number is a palindrome by keep dividing it by 10
+# and get the remainder
+def is_palindrome(number):
+	return True if str(number) == str(number)[::-1] else False
+
+# Keep multiplying 2 3-digit numbers until we find a largest palindrome
+def main():
+    # currPalindrome: store product of the current multiplication which is a palindrome
+    # palindrome: 		store the largest palindrome we found so far
+    # Do not use list for memory efficiency
+    result = -1
+
+    # Use 2 for loop, multiply all numbers between 999 and 100 inclusively
+    # Check for palindrome after each multiplication
+    for first in range(999, 99, -1):
+        for second in range(999, 99, -1):
+            product = first * second
+            if is_palindrome(product) and result < product:
+                result = product
+
+    # Print only the largest palindrome
+    print (result)
+
+if __name__ == "__main__":
+    main()
