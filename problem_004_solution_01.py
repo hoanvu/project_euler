711b7ddcf8ef5bca9075d8b4796561053326cc27
hoanvu <vumaihoan@gmail.com>
2016-06-30 10:53:42 +0700

    update code for project 004 solution 01

--- problem_004_solution_01.py
+++ problem_004_solution_01.py
@@ -9,7 +9,7 @@ def is_palindrome(number):
     :param number: input number to be checked
     :return: True if input number is a palindrome, False otherwise
     """
-	return str(number) == str(number)[::-1]
+    return str(number) == str(number)[::-1]
 
 # Keep multiplying 2 3-digit numbers until we find a largest palindrome
 def main():
@@ -19,9 +19,10 @@ def main():
     result = -1
 
     # Use 2 for loop, multiply all numbers between 999 and 100 inclusively
-    # Check for palindrome after each multiplication
+    # At the start of each second for loop, we assign `second` to the value of `first`
+    # This will reduce roughly half the amount of operations needed
     for first in range(999, 99, -1):
-        for second in range(999, 99, -1):
+        for second in range(first, 99, -1):
             product = first * second
             if is_palindrome(product) and result < product:
                 result = product
